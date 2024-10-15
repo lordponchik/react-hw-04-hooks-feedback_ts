db6c7095b9c2e44d7d445fbe1589e90d1547c6ef
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-10-15 12:15:14 +0200

    add styles

--- src/components/FeedbackOptions/FeedbackOptions.tsx
+++ src/components/FeedbackOptions/FeedbackOptions.tsx
@@ -1,3 +1,5 @@
+import s from './FeedbackOptions.module.css';
+
 interface Props {
   goodFeedback: number;
   neutralFeedback: number;
@@ -12,18 +14,18 @@ export default function FeedbackOptions({
   onLeaveFeedback,
 }: Props) {
   return (
-    <ul>
-      <li key="good">
+    <ul className={s.feedbacks}>
+      <li key="good" className={s.feedback}>
         <button type="button" name="good" onClick={onLeaveFeedback}>
           good
         </button>
       </li>
-      <li key="neutral">
+      <li key="neutral" className={s.feedback}>
         <button type="button" name="neutral" onClick={onLeaveFeedback}>
           neutral
         </button>
       </li>
-      <li key="bad">
+      <li key="bad" className={s.feedback}>
         <button type="button" name="bad" onClick={onLeaveFeedback}>
           bad
         </button>
--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,3 +1,5 @@
+import s from './Section.module.css';
+
 interface Props {
   title?: string;
   children: React.ReactNode;
@@ -5,8 +7,8 @@ interface Props {
 
 export default function Section({ title, children }: Props) {
   return (
-    <section>
-      <h2>{title}</h2>
+    <section className={s.section}>
+      <h2 className={s.title}>{title}</h2>
       {children}
     </section>
   );
